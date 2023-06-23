00cf8924308d6b3caa8ec9a5988fdb682844d349
Emmy11111 <73791738+Emmy11111@users.noreply.github.com>
2023-06-23 15:34:52 +0200

    first

---
+++ util/colors.js
@@ -0,0 +1,15 @@
+const colors = {
+    primary: "#009E49",
+    white: "#ffff",
+    navy: "#223263",
+    black: "#000",
+    light: "#9098B1",
+    lightest: "#EBF0FF",
+    default: '#F7941D',
+    green: '#25d482',
+    border: '#9098b2',
+    borderBottomColor: "#DADADA",
+    error: "#F43A4D"
+}
+
+export default colors;
